4420fe2382654be028273016d012492f484a903e
Kevin Studer <kevin.studer@id.unibe.ch>
2018-09-12 12:15:57 +0200

    Added index controller

--- src/main/java/ch/kreemer/workshopdemo2/DemoController.java
+++ src/main/java/ch/kreemer/workshopdemo2/DemoController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class DemoController {
 
+    @GetMapping("/")
+    public String index(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
